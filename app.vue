d9cf0e86e352d53ad8364e359d8f545863503691
Joe Robinson <joe.rob.dev@gmail.com>
2023-11-11 07:48:56 -0700

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-slate-900 h-screen w-screen flex justify-center items-center">
+    <div class="max-w-sm w-full bg-gray-600 relative">
+
+        <div>
+          <div class="w-full h-[200px]">
+          <NuxtImg src="https://source.unsplash.com/random?landscape" class="w-full h-full object-cover"/>
+        </div>
+
+        <div class="flex flex-col items-center gap-8 relative bg-transparent top-[-75px]">
+          <NuxtImg src="https://source.unsplash.com/random?profile"  class="w-[150px] h-[150px] rounded-full object-cover mx-auto"/>
+          <div class="text-center text-gray-100">
+            <p class="text-4xl font-semibold mb-2">Joseph Robinson</p>
+            <p class="text-2xl">Software Developer</p>
+          </div>
+          <div class="my-8 w-full px-8">
+            <button class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Save My Contact
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBAs87AEuFcC21ytlaciU4QxRn8CoA9gcE",
+authDomain: "v-card-98dd9.firebaseapp.com",
+projectId: "v-card-98dd9",
+storageBucket: "v-card-98dd9.appspot.com",
+messagingSenderId: "651293544881",
+appId: "1:651293544881:web:8f0555d616c49c4349ed90"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
